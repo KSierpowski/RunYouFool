1f21331fdb9504d7ea8f6a34d1f5c97d3063ab85
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-10-05 12:50:17 +0200

    Add game over sequence

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+
+    [SerializeField] Canvas endGameCanvas;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] ParticleSystem explosionParticle;
+
+    public bool endGame = false;
+
+    private void Start()
+    {
+        endGameCanvas.enabled = false;
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            endGame = true;
+            StartCoroutine(EndGame());           
+        }
+    }
+    private IEnumerator EndGame()
+    {
+        explosionParticle.Play();
+        GetComponent<Movement>().enabled = false;
+        yield return new WaitForSeconds(1f);
+        endGameCanvas.enabled = true;
+        yield return null;
+    }
+}
