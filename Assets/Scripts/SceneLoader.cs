34f074c427125c340e8d29869a627ffcb0bf2f03
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-10-05 12:12:50 +0200

    Added scene loader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+
+        Time.timeScale = 1;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
