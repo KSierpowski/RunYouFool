a3a36353b5f61b509e6e95da7ec94be6c527f8c0
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-10-10 10:54:00 +0200

    Spawn bonus at random

---
+++ Assets/Scripts/BonusSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawner : MonoBehaviour
+{
+    public GameObject[] objectsToSpawn;
+    public Transform[] spawnPoints;
+    public List<GameObject> spawnedObjects; 
+    public int spawnCount; 
+    private int objectIndex; 
+    private int spawnIndex;
+    private void Start()
+    {
+        StartCoroutine(RandomSpawn());
+    }
+
+    public void Spawn()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            objectIndex = Random.Range(0, objectsToSpawn.Length);
+            spawnIndex = Random.Range(0, spawnPoints.Length);
+            GameObject spawn = Instantiate(objectsToSpawn[objectIndex], spawnPoints[spawnIndex].position, Quaternion.identity);
+            spawnedObjects.Add(spawn);
+        }
+    }
+    IEnumerator RandomSpawn()
+    {
+        while(spawnCount > 0)
+        {
+            Spawn();
+            yield return new WaitForSeconds(30f);
+        }
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            Gizmos.DrawSphere(spawnPoints[i].position, 0.5f);
+        }
+    }
+}
