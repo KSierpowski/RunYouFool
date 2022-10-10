44db49d9464c2ebb1674a0bc7b90ace66189b417
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-10-10 13:38:45 +0200

    If player fails to catch the bonus, it will be destroyed

--- Assets/Scripts/DestroyBonus.cs
+++ Assets/Scripts/DestroyBonus.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class DestroyBonus : MonoBehaviour
 {
+    public float lifeTime = 5f;
+    private void Start()
+    {
+        StartCoroutine(DestroyAfterTime());
+    }
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
         }
+    }
+    IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
     }
 }
