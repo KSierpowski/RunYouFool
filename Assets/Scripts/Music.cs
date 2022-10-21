5761c8d126bcd611529512cf48ff6a0786fb74e9
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-10-21 14:30:17 +0200

    Added music and mute switcher

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,42 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] Canvas muteImage;
+    [SerializeField] AudioClip mainSong;
+    public bool mute = false;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        muteImage.enabled = false;
+    }
+    private void Update()
+    {
+        Mute();
+    }
+
+    private void Mute()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+
+            mute = !mute;
+            if (mute == false)
+            {
+                audioSource.mute = false;
+                muteImage.enabled = false;
+            }
+            else
+            {
+                audioSource.mute = true;
+                muteImage.enabled = true;
+            }
+        }
+    }
+}
